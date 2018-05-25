56392387c8315cc8b153f93b452b3f166e10c62e
Minh Tran <minh.tran@s3corp.com.vn>
2018-05-25 13:58:43 +0700

    second update

--- API/models/demomodel.js
+++ API/models/demomodel.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var demomodelschema = new Schema({
+
+/*var demomodelschema = new Schema({
 name:{
  type:String,
  required:false
@@ -11,6 +12,8 @@ description:{
 }
 });
 
+*/
+
 var BookSchema = mongoose.Schema({
     name: String,
     price: Number,
@@ -20,19 +23,30 @@ var BookSchema = mongoose.Schema({
   
   var demomodel = mongoose.model('demomodel', BookSchema,'bookstore18');
 
+  
 // The session about the inserting of the new data to the database, this is executed once from the first run.
 // Check the database for the verification
 
-
-  var book1 = [{ name: 'Introduction to Mongoose', price: 20, quantity: 25, condition: 'good' },
+//
+  var book1 = [{ name: 'Introduction to Mongoose', price: 35, quantity: 25, condition: 'good' },
   
   { name: 'Introduction to Python', price: 10, quantity: 25, condition: 'good' },
 
-  { name: 'Introduction to Mongoose javascript', price: 10, quantity: 30, condition: 'good' },
+  { name: 'Introduction to Mongoose javascript', price: 10, quantity: 50, condition: 'good' },
 
-  { name: 'Introduction to Mongoose Nodejs', price: 20, quantity: 30, condition: 'good' },
+  { name: 'Introduction to Mongoose Nodejs', price: 20, quantity: 30, condition: 'used' },
 
-  { name: 'Introduction to JSON', price: 20, quantity: 30, condition: 'good' },
+  { name: 'Introduction to JSON', price: 40, quantity: 30, condition: 'good' },
+
+  { name: 'Introduction to Golang', price: 20, quantity: 15, condition: 'fairly good' },
+
+  { name: 'Javascript for Dummies', price: 20, quantity: 30, condition: 'exellent' },
+
+  { name: 'Python in Action', price: 20, quantity: 30, condition: 'exellent' },
+
+  { name: 'How much is the book Excel for talent?', price: 20, quantity: 30, condition: 'exellent' },
+
+  { name: 'Introduction to Excel', price: 20, quantity: 30, condition: 'exellent' },
 
   ];
   
@@ -44,5 +58,8 @@ demomodel.collection.insert(book1, function (err, docs) {
   }
 });
 
+// End of last session
+
+//
 
 module.exports = demomodel;
